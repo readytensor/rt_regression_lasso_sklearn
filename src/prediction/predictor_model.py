ccaa5cd37158d53e522770c6e5c7c6ca0108eb83
moo3030 <mohamedabdelhamid3030@gmail.com>
2023-10-26 23:24:12 +0200

    Updated default value of alpha

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -25,13 +25,13 @@ class Regressor:
 
     def __init__(
         self,
-        alpha: Optional[float] = 1.0,
+        alpha: Optional[float] = 0.5,
         **kwargs,
     ):
         """Construct a new Lasso regressor.
 
         Args: Constant that multiplies the L1 term, controlling regularization strength. alpha must be a non-negative
-        float i.e. in [0, inf) Defaults to 1.0
+        float i.e. in [0, inf) Defaults to 0.5
         """
         self.alpha = float(alpha)
         self.model = self.build_model()
